64dd40fb199ec4ea5c3673dcbcaee933f0b47cc6
juanignaciomorales <juanignaciomorales2015@gmail.com>
2026-06-18 21:56:57 -0300

    modificaciones

--- Proyecto-React/src/components/App.jsx
+++ Proyecto-React/src/components/App.jsx
@@ -1,10 +1,13 @@
 import '..components/App.css'
+import Button from './Button.jsx';
+import Counter from './Counter';
+import Users from './Users';
 
 function App() {
 
   return 
-   
-  
+ Counter
+ Button
+ Users
 }
-
-export default App
+export default App;
---
+++ Proyecto-React/src/components/Counter.jsx
@@ -0,0 +1,9 @@
+const Counter = () => {
+    return (
+      <>
+        Counter
+        </>
+    )
+}
+
+export default Counter;
